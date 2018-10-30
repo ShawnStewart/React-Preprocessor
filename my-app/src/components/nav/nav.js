f7be3d3ec06e95ee3db575543dfe8228aadf9084
Shawn Stewart <s.stewart0220@gmail.com>
2018-10-30 05:23:54 -0400

    Nav is keeping track of active menu item

--- my-app/src/components/nav/nav.js
+++ my-app/src/components/nav/nav.js
@@ -1,18 +1,54 @@
 import React, { Component } from "react";
 
 export default class nav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: ""
+    };
+  }
+
+  handleMenuClick = name => {
+    this.setState({ active: name });
+  };
+
   render() {
     return (
       <nav className="nav container">
         <ul>
           <li>
-            <a href="#">Home</a>
+            <a
+              href="localhost:3000"
+              className={this.state.active === "Home" ? "active" : ""}
+              onClick={() => {
+                this.handleMenuClick("Home");
+              }}
+            >
+              Home
+            </a>
           </li>
           <li>
-            <a href="#">About</a>
+            <a
+              href="localhost:3000"
+              className={this.state.active === "About" ? "active" : ""}
+              onClick={() => {
+                this.handleMenuClick("About");
+              }}
+            >
+              About
+            </a>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <a
+              href="localhost:3000"
+              className={this.state.active === "Contact" ? "active" : ""}
+              onClick={() => {
+                this.handleMenuClick("Contact");
+              }}
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
