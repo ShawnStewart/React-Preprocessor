8b1302844b31f27acea3b393805fc222d494be35
Shawn Stewart <s.stewart0220@gmail.com>
2018-10-29 23:32:57 -0400

    Created a nav bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import "./stylesheets/main.css";
 import Test from "./components/test1/test1";
 import Test2 from "./components/test2/test2";
+import Nav from "./components/nav/nav";
 
 class App extends Component {
   render() {
     return (
       <div className="main">
+        <Nav />
         <Test />
         <Test2 />
       </div>
