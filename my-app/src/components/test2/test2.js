d1f58861df8dbf0d8a6d8bf9c256f3743133a7c6
Shawn Stewart <s.stewart0220@gmail.com>
2018-11-01 18:31:50 -0400

    Updated button styling

--- my-app/src/components/test2/test2.js
+++ my-app/src/components/test2/test2.js
@@ -4,19 +4,22 @@ export default function test2() {
   return (
     <div className="test2">
       <span>
-        <button className="btn small primary">button</button>
+        <button className="btn small primary">Button</button>
       </span>
       <span>
-        <button className="btn small secondary">button</button>
+        <button className="btn small secondary">Button</button>
       </span>
       <span>
-        <button className="btn">button</button>
+        <button className="btn">Button</button>
       </span>
       <span>
-        <button className="btn big blue">button</button>
+        <button className="btn big blue">Button</button>
       </span>
       <span>
-        <button className="btn huge red inverted">button</button>
+        <button className="btn huge red inverted">Button</button>
+      </span>
+      <span>
+        <button className="inverted primary btn">Button</button>
       </span>
     </div>
   );
