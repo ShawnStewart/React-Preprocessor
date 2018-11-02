338bda95f03ad55f098e6cbc81c26e20cbf7afd6
Shawn Stewart <s.stewart0220@gmail.com>
2018-11-01 21:04:12 -0400

    Navbar has a hamburger menu for mobile

--- my-app/src/components/nav/nav.js
+++ my-app/src/components/nav/nav.js
@@ -5,7 +5,8 @@ export default class nav extends Component {
     super(props);
 
     this.state = {
-      active: ""
+      active: "",
+      showMobileMenu: false
     };
   }
 
@@ -14,43 +15,85 @@ export default class nav extends Component {
   };
 
   render() {
+    console.log(this.state);
     return (
-      <nav className="nav container">
-        <ul>
-          <li>
-            <a
-              href="localhost:3000"
-              className={this.state.active === "Home" ? "active" : ""}
+      <nav className="nav">
+        <div className="menu-bar container">
+          <h1 className="menu-bar-logo">Arcelona Design</h1>
+          <span
+            className="hamburger"
+            onClick={() =>
+              this.setState({ showMobileMenu: !this.state.showMobileMenu })
+            }
+          >
+            <div className="bar1" />
+            <div className="bar2" />
+            <div className="bar3" />
+          </span>
+          <ul className="desktop-menu">
+            <li>
+              <a
+                href="localhost:3000"
+                className={this.state.active === "Home" ? "active" : ""}
+                onClick={() => {
+                  this.handleMenuClick("Home");
+                }}
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="localhost:3000"
+                className={this.state.active === "About" ? "active" : ""}
+                onClick={() => {
+                  this.handleMenuClick("About");
+                }}
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="localhost:3000"
+                className={this.state.active === "Contact" ? "active" : ""}
+                onClick={() => {
+                  this.handleMenuClick("Contact");
+                }}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        {this.state.showMobileMenu ? (
+          <div className="mobile-menu">
+            <div
+              className="mobile-menu-item"
               onClick={() => {
                 this.handleMenuClick("Home");
               }}
             >
               Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="localhost:3000"
-              className={this.state.active === "About" ? "active" : ""}
+            </div>
+            <div
+              className="mobile-menu-item"
               onClick={() => {
                 this.handleMenuClick("About");
               }}
             >
               About
-            </a>
-          </li>
-          <li>
-            <a
-              href="localhost:3000"
-              className={this.state.active === "Contact" ? "active" : ""}
+            </div>
+            <div
+              className="mobile-menu-item"
               onClick={() => {
                 this.handleMenuClick("Contact");
               }}
             >
               Contact
-            </a>
-          </li>
-        </ul>
+            </div>
+          </div>
+        ) : null}
       </nav>
     );
   }
